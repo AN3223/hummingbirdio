6b9d6a2a102a26206af7fe88f47969ef49e9ce0a
AN3223 <35916076+AN3223@users.noreply.github.com>
2018-04-23 01:09:18 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hummingbirdio',
+    version='0.1.0',
+    description='',
+    long_description='',
+    url='',
+    author='AN3223',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+)
